a9170ab8c4e3d695aeb4d43a1e12ef98f4ac109b
Nam Dinh <hakinam2701@gmail.com>
2025-01-13 09:41:34 +0700

    add ads

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -65,40 +65,30 @@ function Home() {
             <Row>
                 {/* Main Content */}
                 <Col xs={12} md={7} lg={8} className="main-content">
-                    {/*<div className="m-lg-4">*/}
-                    {/*    <ImageBanner id="banner"/>*/}
-                    {/*</div>*/}
-                    {/*<div className="m-lg-4 w-100 d-flex justify-content-center align-items-center">*/}
-                    {/*    <Ads src="//www.highperformanceformat.com/fd65adebc0efc4af9e9d464f80cdf4df/invoke.js"*/}
-                    {/*         keyId="fd65adebc0efc4af9e9d464f80cdf4df" width="720" height="90" delay={0}/>*/}
-                    {/*</div>*/}
+                    <div className="m-lg-4 w-100 d-flex justify-content-center align-items-center">
+                        <Ads src="//www.highperformanceformat.com/2b5f21228bdc2629b2e1d6876eb02692/invoke.js"
+                             keyId="2b5f21228bdc2629b2e1d6876eb02692" width="728" height="90" delay={0}/>
+                    </div>
                     <div className="m-lg-4" id="channel-list-section">
-                        <ChannelList channels={channelsFilter} readMore={true}/>
+                        <ChannelList channels={channelsFilter} readMore={false}/>
                     </div>
                 </Col>
 
                 {/* Sidebar */}
                 <Col xs={12} md={5} lg={4} className="sidebar d-flex flex-column align-items-center">
-                    {/*<div className="m-lg-4" id="news-section">*/}
-                    {/*    <News/>*/}
-                    {/*</div>*/}
-                    {/*<div className="m-lg-4" id="news-section">*/}
-                    {/*    <Ads src="//www.highperformanceformat.com/d79a19eb1b1b285b56d1d558b74a5707/invoke.js"*/}
-                    {/*         keyId="d79a19eb1b1b285b56d1d558b74a5707" width="300" height="250" delay={500}/>*/}
-                    {/*</div>*/}
-                    {/*<div className="m-lg-4" id="news-section">*/}
-                    {/*    <Ads src="//www.highperformanceformat.com/d79a19eb1b1b285b56d1d558b74a5707/invoke.js"*/}
-                    {/*         keyId="d79a19eb1b1b285b56d1d558b74a5707" width="300" height="250" delay={1000}/>*/}
-                    {/*</div>*/}
-                    {/*<div className="m-lg-4" id="news-section">*/}
-                    {/*    <Ads src="//www.highperformanceformat.com/d79a19eb1b1b285b56d1d558b74a5707/invoke.js"*/}
-                    {/*         keyId="d79a19eb1b1b285b56d1d558b74a5707" width="300" height="250" delay={1500}/>*/}
-                    {/*</div>*/}
+                    <div className="m-lg-4" id="news-section">
+                        <Ads src="//www.highperformanceformat.com/a5318143ea79bef2886adfb6cd93a6f4/invoke.js"
+                             keyId="a5318143ea79bef2886adfb6cd93a6f4" width="300" height="250" delay={500}/>
+                    </div>
+                    <div className="m-lg-4" id="news-section">
+                        <Ads src="//www.highperformanceformat.com/a5318143ea79bef2886adfb6cd93a6f4/invoke.js"
+                             keyId="a5318143ea79bef2886adfb6cd93a6f4" width="300" height="250" delay={1000}/>
+                    </div>
+                    <div className="m-lg-4" id="news-section">
+                        <Ads src="//www.highperformanceformat.com/a5318143ea79bef2886adfb6cd93a6f4/invoke.js"
+                             keyId="a5318143ea79bef2886adfb6cd93a6f4" width="300" height="250" delay={1500}/>
+                    </div>
 
-
-                    {/*<div className="m-lg-4" id="featured-articles-section">*/}
-                    {/*    <FeaturedArticles/>*/}
-                    {/*</div>*/}
                 </Col>
             </Row>
         </Container>
