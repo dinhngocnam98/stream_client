e6782cde160169f183cc5402ba35b8498cb25e4e
Nam Dinh <hakinam2701@gmail.com>
2025-01-25 18:31:59 +0700

    add ads sexy68

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,6 +4,7 @@ import {useEffect, useState} from "react";
 import {fetchChannels} from "../store/actions/channelActions";
 import {Helmet, HelmetProvider} from "react-helmet-async";
 import {Col} from "react-bootstrap";
+import Ads from "./Ads";
 
 function Home() {
     const dispatch = useDispatch();
@@ -35,6 +36,13 @@ function Home() {
             </HelmetProvider>
             <Col xs={12} sm={12} md={12} lg={12}>
                 <div className="m-lg-5">
+                    <div className="m-lg-4 w-100 d-flex justify-content-center align-items-center">
+                        <Ads
+                            src="\/\/forsakenburn.com\/b\/XqV.s\/dmGulA0\/YsWkcf\/seVmN9\/u\/Z\/UulekUPlT\/YKw\/M\/zfEU1_MLjLMItZNWjOALzXM\/T\/U\/yvNqAr"
+                            zoneId='nkn6q3njsq'
+                            delay={0}
+                        />
+                    </div>
                     <ChannelList scroll={isScrollable}/>
                 </div>
             </Col>
